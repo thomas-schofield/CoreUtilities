f6b46640424b3c8912ba1cca56594cb75d40cba1
Thomas Schofield <thomas.schofield@ieee.org>
2024-09-20 16:47:20 -0400

    update install and generate scripts

--- conan_install.sh
+++ conan_install.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
+cd $(git rev-parse --show-toplevel -q)
+
+if [ $? -ne 0  ]; then
+  echo 'Failed to change directory'
+fi
+
 conan install . --output-folder=build --build=missing
--- generate_cmake.sh
+++ generate_cmake.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
+cd $(git rev-parse --show-toplevel -q)
+if [ $? -ne 0 ]; then
+  echo 'Failed to change directory'
+fi
 
 cd build
+
 cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
 
