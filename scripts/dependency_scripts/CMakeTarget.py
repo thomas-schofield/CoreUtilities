715f9afe3992950347a17ddd2885694d1fe2a7b2
Thomas Schofield <thomas.schofield@ieee.org>
2024-12-14 15:48:27 -0500

    Separate function for CMakeTarget source files

--- scripts/dependency_scripts/CMakeTarget.py
+++ scripts/dependency_scripts/CMakeTarget.py
@@ -8,97 +8,133 @@ import CMakeParser
 
 class CMakeTarget:
     """Retrieve information about a CMake target"""
-    def __init__(self, target_file: str):
-        self.parser = CMakeParser.CMakeParser(target_file)
-        self.cache = True
+    def __init__(self, target_file: str, use_cache=True):
+        self.cache = use_cache
+        self.parser = CMakeParser.CMakeParser(target_file,
+                                              use_cache=self.cache)
 
+        self.source_files = []
         self.includes = []
 
     def __str__(self):
-        if self.isValidTarget():
-            return self.getTargetName()
-        return f"Invalid target: {self.parser.getTargetFile()}"
+        return f"Target: {self.getTargetName()}, Includes: {self.includes}"
 
     def setUseCache(self, state: bool) -> None:
+        """Define cache usage for class"""
         self.cache = state
         self.parser.setUseCache(state)
 
     def getTargetName(self) -> str:
+        """Retrieve target name via CMakeParser"""
         return self.parser.getTargetName()
 
     def getTargetType(self) -> CMakeParser.CMakeTargetType:
+        """Retrieve target type via CMakeParser"""
         return self.parser.getTargetType()
 
     def getTargetDependencies(self) -> list[str]:
+        """Retrieve target dependencies via CMakeParser"""
         return self.parser.getTargetDependencies()
 
     def isValidTarget(self) -> bool:
-        try:
-            return self.getTargetType() == CMakeParser.CMakeTargetType.LIBRARY
-        except Exception as e:
-            print(e)
-            return False
-
-    def getTargetIncludes(self) -> list[str]:
+        return self.getTargetType() != CMakeParser.CMakeTargetType.INVALID
+    
+    def getSourceFiles(self) -> list[str]:
+        """Find source files for the CMake target"""
         if not self.isValidTarget():
             return []
-        
+
+        if self.cache and self.source_files:
+            return self.source_files
+
+        target_dir = self.parser.getTargetDirectory()
+        for (root, _, files) in os.walk(target_dir):
+            # Ignore cmake-related directories
+            if "cmake" in root.lower():
+                continue
+
+            rel_dir = os.path.relpath(root, target_dir)
+            for _file in files:
+                # Ignore cmake-related files
+                if "cmake" in _file.lower():
+                    continue
+
+                # Ignore unit test-related files
+                if "test" in _file.lower():
+                    continue
+
+                selected_file = os.path.join(rel_dir, _file)
+                # Get rid of ./
+                if selected_file.startswith("./"):
+                    selected_file = selected_file[2:]
+
+                if selected_file not in self.source_files:
+                    self.source_files.append(selected_file)
+
+        return self.source_files
+
+    def getExternalIncludes(self) -> list[str]:
+        """Parses source files and returns external target headers"""
+        if not self.isValidTarget():
+            return []
+
         if self.cache and self.includes:
             return self.includes
 
-        target_files = os.listdir(self.parser.getTargetDirectory())
-        for entry in target_files:
-            target_file = os.path.join(self.parser.getTargetDirectory(), entry)
-            if not os.path.isfile(target_file):
-                continue
+        # Ensure source files are read
+        self.getSourceFiles()
 
-            if "cmake" in entry.lower():
-                continue
-
-            with open(target_file, "r") as f:
+        for _file in self.source_files:
+            with open(os.path.join(self.parser.getTargetDirectory(), _file), "r", encoding="utf-8") as f:
                 for line in f:
                     if "#include" not in line:
                         continue
 
-                    pattern = re.escape("<") + "(.*)" + re.escape(">")
-                    p = re.compile(pattern)
-                    system_include = p.findall(line)
-                    if system_include and system_include[0] not in target_files: 
-                        system_include = system_include[0]
-                        if system_include not in self.includes:
-                            self.includes.append(system_include)
-                            continue
+                    # remove relative includes
+                    if ".." in line:
+                        continue
 
                     pattern = re.escape('"') + "(.*)" + re.escape('"')
                     p = re.compile(pattern)
                     local_include = p.findall(line)
-                    if local_include and local_include[0] not in target_files:
+                    if local_include and local_include[0] not in self.source_files:
                         local_include = local_include[0]
                         if local_include not in self.includes:
                             self.includes.append(local_include)
                             continue
 
+                    pattern = re.escape("<") + "(.*)" + re.escape(">")
+                    p = re.compile(pattern)
+                    system_include = p.findall(line)
+                    if system_include and system_include[0] not in self.source_files:
+                        system_include = system_include[0]
+                        if system_include not in self.includes:
+                            self.includes.append(system_include)
+                            continue
+
         self.includes.sort()
         return self.includes
 
-    def resolveDependencies(self, external_projects: list[str]) -> list[str]:
-        includes = self.getTargetIncludes()
-
-        raise NotImplementedError
-
 
 if __name__ == "__main__":
-    external_dependency_file = sys.argv[1]
-    external_dependency_file_full_path = os.path.join(os.getcwd(), external_dependency_file)
+    for arg in sys.argv[1:]:
+        try:
+            target = CMakeTarget(arg)
 
-    for arg in sys.argv[2:]:
-        project = CMakeTarget(arg)
+            if not target.isValidTarget():
+                print(f"No valid target found for: {arg}")
+                continue
+            
+            name = target.getTargetName()
+            print(f"Reading data for target: {name}")
 
-        includes = project.getTargetIncludes()
-        if includes:
-            print(f"Found includes: {includes}")
-        else:
-            print(f"No includes found for: {arg}")
-        
-        # external_dependencies = project.getAllExternalTargets(external_dependency_file_full_path)
-        # print(f"Retrieved external dependencies: {external_dependencies}")
+            sources = target.getSourceFiles()
+            print(f"Source files for {name}: {sources}")
+
+            external_includes = target.getExternalIncludes()
+            if external_includes:
+                print(f"Found external includes: {external_includes}")
+            else:
+                print(f"No includes found for: {arg}")
+        except Exception as e:
+            print(f"Error: {e}")
